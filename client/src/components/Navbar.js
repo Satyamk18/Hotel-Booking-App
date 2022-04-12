5cbff42386642998678595582f6861c0aecfb64c
Satyamk18 <satyvk18@gmail.com>
2022-04-12 22:31:22 +0530

    test

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+
 function Navbar() {
   return (
   <div>
      <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-       <a class="navbar-brand" href="#">Suman's Inn</a>
+       <a class="navbar-brand" href="/#">App Name</a>
        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon"></span>
        </button>
